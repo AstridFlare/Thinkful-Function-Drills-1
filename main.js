5d4548c3869a3ac20b6eff1fc1ea8a0ce85d6c50
Michael Romero <michael.romero09@gmail.com>
2019-04-29 12:47:14 -0700

    Handled age errors

--- main.js
+++ main.js
@@ -1,15 +1,21 @@
 "use strict";
 
 function getYearOfBirth (age) {
+  if (age < 0) {
+    throw new Error('Age can not be negative');
+  }
   const yearOfBirth = 2019 - age;
   return yearOfBirth;
 }
 
 function createGreeting(name, age) {
-const yob = getYearOfBirth(age);
+  const yob = getYearOfBirth(age);
   return `Hi, my name is ${name} and I am ${age} years old. I was born in ${yob}.`;
 }
 
-const greeting1 = createGreeting("Chris", "29");
-console.log(greeting1);
-
+try {
+  const greeting1 = createGreeting("Chris", "29");
+  console.log(greeting1);
+} catch(e) {
+  console.log(e.message);
+};
