1a24698714d6b81d47fae591227ce9776a423b05
AstridFlare <AstridFlare@Gmail.com>
2019-04-29 12:55:13 -0700

    e Please enter the commit message for your changes. Lines starting

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 "use strict";
 
-function getYearOfBirth (age) {
+function getYearOfBirth(age) {
   if (age < 0) {
-    throw new Error('Age can not be negative');
+    throw new Error("Age can not be negative");
   }
+
   const yearOfBirth = 2019 - age;
   return yearOfBirth;
 }
 
 function createGreeting(name, age) {
   const yob = getYearOfBirth(age);
-  return `Hi, my name is ${name} and I am ${age} years old. I was born in ${yob}.`;
+  if (name === NaN || age === undefined) {
+    throw new Error("You did not enter a valid name or age.");
+  } else
+    return `Hi, my name is ${name} and I am ${age} years old. I was born in ${yob}.`;
 }
 
 try {
   const greeting1 = createGreeting("Chris", "29");
   console.log(greeting1);
-} catch(e) {
+} catch (e) {
   console.log(e.message);
-};
+}
