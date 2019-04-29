a9b4e212f8f91272d2bb61a8c52bb7e4f5c6151f
iAmDusan <dusaniwskyj@gmail.com>
2019-04-29 15:25:10 -0400

    change to return string

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 function createGreeting() {
-  console.log("Hi, my name is chris and I am 29 years old");
+  return "Hi, my name is chris and I am 29 years old";
 }
 
-createGreeting();
+const greeting1 = createGreeting();
+console.log(greeting1);
