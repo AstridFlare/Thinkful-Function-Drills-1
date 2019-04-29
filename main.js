48b1b36e1a619503aa5a0ac32158f764140b2113
iAmDusan <dusaniwskyj@gmail.com>
2019-04-29 16:06:18 -0400

    check that name is string and age is number

--- main.js
+++ main.js
@@ -13,12 +13,17 @@ function createGreeting(name, age) {
   const yob = getYearOfBirth(age);
   if (name === NaN || age === undefined) {
     throw new Error("You did not enter a valid name or age.");
-  } else
+  } 
+  else if (typeof name !== 'string' || typeof age !== 'number') {
+    throw new Error("Name must be a string. Age must be a number")
+  }
+    else {
     return `Hi, my name is ${name} and I am ${age} years old. I was born in ${yob}.`;
+}
 }
 
 try {
-  const greeting1 = createGreeting("Chris", "29");
+  const greeting1 = createGreeting("Chris", 29);
   console.log(greeting1);
 } catch (e) {
   console.log(e.message);
